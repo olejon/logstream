1cae6705ac8067f5d0f9519f6b9a1ac64b6dc80b
Ole Jon Bjørkum <olejon@ion>
2015-10-19 18:54:18 +0200

    Version 1.3

--- logstream/js/main.js
+++ logstream/js/main.js
@@ -1,21 +1,19 @@
 /*
 
-Copyright 2015 Ole Jon Bjørkum
+Copyright (C) 2015 Ole Jon Bjørkum
 
-This file is part of LogStream.
-
-LogStream is free software: you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-LogStream is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LogStream.  If not, see <http://www.gnu.org/licenses/>.
+along with this program. If not, see http://www.gnu.org/licenses/.
 
 */
 
@@ -53,7 +51,7 @@ $(window).load(function()
 
 		return;
 	}
-	else if(isNaN(config.interval) || parseInt(config.interval) < 2)
+	else if(!config.interval.length || isNaN(config.interval) || parseInt(config.interval) < 2)
 	{
 		error.html('Interval must be integer > 1').parent().show();
 
@@ -211,7 +209,7 @@ function lookupIpAddress(ip)
 
 	div.removeClass('fadein');
 
-	$.getJSON('http://www.telize.com/geoip/'+ip+'?callback=?', function(data)
+	$.getJSON('https://www.telize.com/geoip/'+ip+'?callback=?', function(data)
 	{
 		var country = (typeof data.country == 'undefined' || data.country == '') ? 'Unknown' : data.country;
 		var country_code = (typeof data.country_code == 'undefined' || data.country_code == '') ? 'Unknown' : data.country_code.toLowerCase();
